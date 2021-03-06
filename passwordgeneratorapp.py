150c62635236c53cf3a2a7beb692dc914c541bc4
José Tarín <77952401+JoseTarinT@users.noreply.github.com>
2021-03-06 17:35:42 +1100

    Update passwordgeneratorapp.py

--- passwordgeneratorapp.py
+++ passwordgeneratorapp.py
@@ -16,25 +16,26 @@ def password():
     passwordb = "".join(secrets.choice(b) for x in range(lenght))
     passwordc = "".join(secrets.choice(c) for x in range(lenght))
 
+    # This is how the checkboxes work
     if (var1.get() == 1) and (var2.get() == 0) and (var3.get() == 0):
         t1.delete("1.0", END)  # Deletes the content of the Text box when user input changes
         t1.insert(END, passworda)
     elif (var2.get() == 1) and (var1.get() == 0) and (var3.get() == 0):
-        t1.delete("1.0", END)  # Deletes the content of the Text box when user input changes
+        t1.delete("1.0", END) 
         t1.insert(END, passwordb)
     elif (var1.get() == 0) and (var2.get() == 0) and (var3.get() == 1):
-        t1.delete("1.0", END)  # Deletes the content of the Text box when user input changes
+        t1.delete("1.0", END) 
         t1.insert(END, passwordc)
     else:
-        t1.delete("1.0", END)  # Deletes the content of the Text box when user input changes
+        t1.delete("1.0", END)  
         t1.insert(END, "Please, select just one option")
 
 # Create a button widget
-# The kilo_converter() function is called when the button is pushed
+# The password() function is called when the button is pushed
 b1 = Button(window, text = "Get password", command = password, height =2, width = 15)
 b1.grid(row = 4, column = 6)
 
-# Create a Label widget with "Kg" as label
+# Create a Label widget asking the number of characters as label
 e0 = Label(window, text = "How many characters do you want in your password?", height =1, width = 45)
 e0.grid(row = 0, column = 0)
 
@@ -42,20 +43,20 @@ var1 = IntVar()
 var2 = IntVar()
 var3 = IntVar()
 
-e1 = Checkbutton(window, text = "Just letters", variable = var1, onvalue=1, offvalue=0) # command=password
+e1 = Checkbutton(window, text = "Just letters", variable = var1, onvalue=1, offvalue=0)
 e1.grid(row = 1, column = 0, sticky = W)
 
-e2 = Checkbutton(window, text = "Numbers and letters", variable = var2, onvalue=1, offvalue=0) # command=password
+e2 = Checkbutton(window, text = "Numbers and letters", variable = var2, onvalue=1, offvalue=0)
 e2.grid(row = 2, column = 0, sticky = W)
 
-e3 = Checkbutton(window, text = "Letters, numbers and special characters", variable = var3, onvalue=1, offvalue=0) # command=password
+e3 = Checkbutton(window, text = "Letters, numbers and special characters", variable = var3, onvalue=1, offvalue=0) 
 e3.grid(row = 3, column = 0, sticky = W)
 
 e_value = StringVar()
 e = Entry(window, textvariable = e_value) # Create an Entry box for users to enter the value
 e.grid(row = 0, column = 6)
 
-# Create three empty text boxes, t1, t2, and t3
+# Create one empty text boxes
 t1 = Text(window, height =1, width = 35)
 t1.grid(row = 4, column = 0)
 
